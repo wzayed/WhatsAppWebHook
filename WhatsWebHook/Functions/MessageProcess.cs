5e023b6be432d028f7a8e3d27c7b0a801bfffa4d
wzayed <wzayed@yahoo.com>
2022-08-28 12:41:44 +0400

    added Image

--- WhatsWebHook/Functions/MessageProcess.cs
+++ WhatsWebHook/Functions/MessageProcess.cs
@@ -12,9 +12,12 @@ namespace WhatsWebHook.Functions
 
             var msg_url = "https://graph.facebook.com/v13.0/100704419435623/messages";
 
-        /*    var client = new RestClient(msg_url);
-            client.Timeout = -1; */
-            
+        // the one with white BK  string img_link = "https://i.imgur.com/0yv0CuQ.png";        
+        //Without the phone Number    string img_link = "https://i.imgur.com/slMpMfI.png";
+            string img_link = "https://i.imgur.com/WBUravr.png";
+
+
+
             var options = new RestClientOptions(msg_url)
             {
                 ThrowOnAnyError = true,
@@ -29,11 +32,10 @@ namespace WhatsWebHook.Functions
             request.AddHeader("Content-Type", "application/json");
             request.AddHeader("Authorization", "Bearer EAALDinrOTh4BAGS3GZAeAjagdrUFzFyE0uXqtZAHKEUUYyK9p9P9VtpsMx3ZApdDWuVLbT0cFkWZCkCWhd9IYv12vkJWFYmZArGxLqE6DQCKEWMYvwBH5RLRsmumZCZCDCT3RnkdUw1HvOkzu9LuObTG493bhSqoSdasoeOxXLWdPeyxe3v3fdYghyLZCZAZBNlPMXkicoTcrmHAZDZD");
 
-
             string sender_name = contact_name;
             sender_name = sender_name == "" ? "." : ",*" + sender_name + "*.";
                 string msg_heart = "Welcome to *Tech Valley*.This is *Walid Zayed* Thanks for contacting us" + sender_name + "\\nYou sent a message of type *" +  msg_type  + 
-                                  "*, from Number *" + from_no + "* and message text  " + msg_body + "\\n" +
+                                  "*, from Number *" + from_no + "* and message text is( " + msg_body + ")\\n" +
                                   "1️⃣ Contact Information\\n" +
                                   "2️⃣ Our Services\\n" + 
                                   "3️⃣ Contact us";
@@ -45,10 +47,10 @@ namespace WhatsWebHook.Functions
                                   "\"context\": {" + "\n" +
                                   "\"message_id\": \"" + msg_id + "\"" + "\n" +
                                   "            }," + "\n" +
-                                  "\"type\": \"text\"," + "\n" +
-                                  "\"text\": {" + "\n" +
-                                  "          \"preview_url\": false," + "\n" +
-                                  "          \"body\": \"" + msg_heart + "\"" + "\n" +
+                                  "\"type\": \"image\"," + "\n" +
+                                  "\"image\": {" + "\n" +                                  
+                                  "          \"caption\": \"" + msg_heart + "\"," + "\n" +
+                                  "          \"link\": \"" + img_link + "\"" + "\n" +
                                   "        }" + "\n" +
                                   " }";
 
@@ -63,3 +65,18 @@ namespace WhatsWebHook.Functions
         }
     }
 }
+/* The last perfectly working text message version
+ *                  var body = "{" + "\n" +
+                                  "\"messaging_product\": \"whatsapp\"," + "\n" +
+                                  "\"recipient_type\": \"individual\"," + "\n" +
+                                  "\"to\": \"" + from_no + "\"," + "\n" +
+                                  "\"context\": {" + "\n" +
+                                  "\"message_id\": \"" + msg_id + "\"" + "\n" +
+                                  "            }," + "\n" +
+                                  "\"type\": \"text\"," + "\n" +
+                                  "\"text\": {" + "\n" +
+                                  "          \"preview_url\": false," + "\n" +
+                                  "          \"body\": \"" + msg_heart + "\"" + "\n" +
+                                  "        }" + "\n" +
+                                  " }";
+ * */
